91027fa65dc46e3f62263c744b8d43e300dc1bbf
dlibland <david.libland@gmail.com>
2024-04-09 11:47:24 -0700

    Added initial no-smoke mnist

--- src/simple_diffusion/fashion_mnist/plotting.py
+++ src/simple_diffusion/fashion_mnist/plotting.py
@@ -3,23 +3,19 @@ import torch
 
 
 def sample_plotter(real: torch.Tensor, fake: torch.Tensor) -> plt.Figure:
-    """Plots 2-d scatter plot of real and fake samples."""
-    fig, ax = plt.subplots()
-    real = real.detach().cpu().numpy()
-    ax.scatter(
-        real[:, 0],
-        real[:, 1],
-        alpha=0.5,
-        label="True",
-        color="blue",
-    )
-    fake = fake.detach().cpu().numpy()
-    ax.scatter(
-        fake[:, 0],
-        fake[:, 1],
-        alpha=0.5,
-        label="Fake",
-        color="red",
-    )
-    ax.legend()
+    """Plots a 5x6 array of real and fake images the first three colums
+    are real images, the last three columns are fake images."""
+    fig, ax = plt.subplots(5, 6, figsize=(12, 10))
+    for i in range(5):
+        for j in range(3):
+            ax[i, j].imshow(
+                real[i * 3 + j].detach().cpu().numpy().squeeze(), cmap="gray"
+            )
+            ax[i, j].axis("off")
+            ax[i, j].set_title("Real")
+            ax[i, j + 3].imshow(
+                fake[i * 3 + j].detach().cpu().numpy().squeeze(), cmap="gray"
+            )
+            ax[i, j + 3].axis("off")
+            ax[i, j + 3].set_title("Fake")
     return fig
